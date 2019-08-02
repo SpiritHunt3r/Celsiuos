c7b65597f2ddf373032c06251a804f7afb4db4bf
Juan Jose A. Castro <juan_jop96@hotmail.com>
2019-08-01 23:16:13 -0600

    aaaa

--- app/src/main/java/com/example/celsiuscloud/Pantallas/avatar_userpickerscreen.java
+++ app/src/main/java/com/example/celsiuscloud/Pantallas/avatar_userpickerscreen.java
@@ -1,66 +1,237 @@
 package com.example.celsiuscloud.Pantallas;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
 
 import com.example.celsiuscloud.Clases.Usuario;
 import com.example.celsiuscloud.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import in.goodiebag.carouselpicker.CarouselPicker;
 
 public class avatar_userpickerscreen extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    private DatabaseReference ref;
     private FirebaseUser firebaseUser;
+    private FirebaseAuth auth;
+    private DatabaseReference ref;
     private Usuario u;
     private SharedPreferences sharedPref;
+    private AlertDialog.Builder builder;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.avatar_userpicker);
-        mAuth = FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
         ref = FirebaseDatabase.getInstance().getReference();
         sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         String nombre = sharedPref.getString("Nombre","");
         String apellidos = sharedPref.getString("Apellidos","");
+
+        u = new Usuario(nombre,apellidos);
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("Avatar Seleccionado");
+        builder.setMessage("Es correcta esta decisión?");
+    }
+
+    private void cretareuser(int avt){
+        u.setAvatar(avt);
+        final String TAG = "a";
         String email = sharedPref.getString("Email","");
         String pass = sharedPref.getString("Pass","");
-        u = new Usuario(nombre,apellidos);
 
+        auth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(avatar_userpickerscreen.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+        /*auth.createUserWithEmailAndPassword(email,pass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            firebaseUser = auth.getCurrentUser();
+
+                            ref.child("Usuarios").child(firebaseUser.getUid()).child("Personal Info").setValue(u);
+                            Toast.makeText(getApplicationContext(),"Usuario creado correctamente",Toast.LENGTH_SHORT).show();
+                            finish();
+                            Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(i);
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"No es posible crear el usuario",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });*/
     }
 
 
-    /*mAuth.createUserWithEmailAndPassword(ActualEmail,ActualPass)
-                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful())
-                            {
-                                firebaseUser = mAuth.getCurrentUser();
 
-                                ref.child("Usuarios").child(firebaseUser.getUid()).child("Personal Info").setValue(n);
-                                Toast.makeText(getApplicationContext(),"Usuario creado correctamente",Toast.LENGTH_SHORT).show();
-                                finish();
-                                Intent i = new Intent(getApplicationContext(),loginscreen.class);
-                                startActivity(i);
-                            }
-                            else{
-                                Toast.makeText(getApplicationContext(),"No es posible crear el usuario",Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });*/
+    public void avatarpick1(View v){
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                cretareuser(R.drawable.avatar1);
+                dialog.dismiss();
+
+            }
+        });
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Do nothing
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void avatarpick2(View v){
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                cretareuser(R.drawable.avatar2);
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Do nothing
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void avatarpick3(View v){
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                cretareuser(R.drawable.avatar3);
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Do nothing
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void avatarpick4(View v){
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                cretareuser(R.drawable.avatar4);
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Do nothing
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void avatarpick5(View v){
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                cretareuser(R.drawable.avatar5);
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Do nothing
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+    public void avatarpick6(View v){
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                cretareuser(R.drawable.avatar6);
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Do nothing
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
 }
